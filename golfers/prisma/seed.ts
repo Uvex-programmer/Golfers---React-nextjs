6a669f7998e7e1618dc198b1cedbe46a6d409c05
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-01-28 09:48:24 +0100

    update schema, testing prisma

--- golfers/prisma/seed.ts
+++ golfers/prisma/seed.ts
@@ -3,10 +3,12 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.user.create({
+  await prisma.golfclub.create({
     data: {
-      email: 'admin2@admin2.com',
-      role: 'ADMIN',
+      image: 'admin2@admin2.com',
+      url: 'https://www.bosjokloster.se/',
+      description:
+        'Bosjökloster GK:s golfbana är ritad av Douglas Brasier och blev spelklar 1974. De första 9 hålen invigdes den 24 augusti 1974 och den 9 augusti 1975 stod alla 18 hålen klara. Banan är av karaktär "Öppen parkbana". Golfbanan har idag, förutom de ursprungliga väl inbunkrade greenerna, fler svårigheter på vägen mellan tee och green än ryktet kanske säger.',
     },
   })
 }
