8bc426dd32c67860c96e0052beeb8ab7f639ee36
Uvex-programmer <77095105+Uvex-programmer@users.noreply.github.com>
2022-01-29 17:40:36 +0100

    login btn

--- golfers/components/header/Header.tsx
+++ golfers/components/header/Header.tsx
@@ -17,6 +17,9 @@ const TopNavbar = () => {
         </li>
         <li className='header__nav-item'>Bokade tider</li>
         <li className='header__nav-item'>Galleri</li>
+        <li className='header__nav-item'>Bli medlem</li>
+        <li className='header__nav-item header__nav-item--white'>
+Logga in</li>
       </>
     )
   }
